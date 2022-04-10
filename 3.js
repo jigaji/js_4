8426775bfe255a6754b04059d2019464a8a73de8
Saglara Tserenova <ts.saglara@gmail.com>
2022-04-10 19:28:24 +0300

    Second commit

--- 3.js
+++ 3.js
@@ -15,40 +15,47 @@ function myLog(file, string) {
 
 let count = 1;
 let n = Math.floor(Math.random() * 1000);
-console.log('Случайное число: '+ n);
 
 
-
-function game (){
-  myLog('game.txt', (`случайное число ${n}\n`));
-  let promise =  new Promise((resolve, reject)=> {
-    rl.question('Угадай число от 0 до 1000\n', function (data){
+async function game() {
+  rl.question ('Угадай число от 0 до 1000\n', function (data){
+    console.log('Случайное число: '+ n);
     data = parseInt(data);
     console.log(`---Попытка ${count}---`);
-    myLog('game.txt', (`Попытка ${count}\n`));
-
-    if (data == n) {
-        console.log ('Поздравляю! Вы угалали число!');
-        myLog('game.txt', (data+'- Поздравляю! Вы угалали число!'));
-        rl.close();
-    } else if (data > n) {
-        console.log('Вы ввели число больше.');
-        myLog('game.txt', (data+'- Вы ввели число больше.'));
-        ++count;
-        game();
-    } else if (data < n) {
-        console.log('Вы ввели число меньше.');
-        myLog('game.txt', (data+'- Вы ввели число меньше.'));
-        ++count;
-        game();
-    } 
+    myLog('game.txt', (`Попытка ${count}\n`)); 
+    evaluateAnswer(data) 
     });
-  });
-  return promise;
 }
 
-let g = game();
+
+function evaluateAnswer(data){
+  if (data == n) {
+    console.log ('Поздравляю! Вы угалали число!');
+    myLog('game.txt', (data+'- Поздравляю! Вы угалали число!'));
+    rl.close();
+  } else if (data > n) {
+    console.log('Вы ввели число больше.');
+    myLog('game.txt', (data+'- Вы ввели число больше.'));
+    ++count;
+    game();
+  } else if (data < n) {
+    console.log('Вы ввели число меньше.');
+    myLog('game.txt', (data+'- Вы ввели число меньше.'));
+    ++count;
+    game();
+  } 
+}
 
 
+async function main() {
+  console.log('Давай сыграем в игру?');
+
+  try {
+    await game();
+  } catch (error) {
+    console.error('Ошибка', error);
+  }
+}
 
 
+main();
